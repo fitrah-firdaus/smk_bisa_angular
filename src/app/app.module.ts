0a109f98f8ec58562b06ed1929e7dd250e280a8c
FEF <fitrah.firdaus@traveloka.com>
2017-10-28 09:32:34 +0700

    Create Customer Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { RailsComponent } from './rails/rails.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    RailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormBuilder,
+    FormGroup,
+    Validators,
+    RouterModule.forRoot(
+      [
+        {
+          path : 'customer',
+          component : CustomerComponent
+        }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rails/rails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rails',
+  templateUrl: './rails.component.html',
+  styleUrls: ['./rails.component.css']
+})
+export class RailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
